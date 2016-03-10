78af59b59808e80f4776dc6bf1ff2f2c095a1741
Byron Wall <byrondevonwall@gmail.com>
2016-03-10 08:21:06 -0500

    add multi-word functionality to @gif

--- assets/js/chat_functions/giphy.js
+++ assets/js/chat_functions/giphy.js
@@ -1,7 +1,7 @@
 
 
 function giphy(){
-   var gifSubject = textArr[0];//this array number may be different
+   var gifSubject = textArr.join('+');//this array number may be different
    $.ajax({
       url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+gifSubject+'',
       method: "GET",
