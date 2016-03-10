a5bcc42fdd7f82a86300802e9c0d0c6914433a25
Byron Wall <byrondevonwall@gmail.com>
2016-03-10 08:47:22 -0500

    Add better help functionality

--- assets/js/chat_functions/help.js
+++ assets/js/chat_functions/help.js
@@ -1,5 +1,6 @@
 //look for @help
 var commands = [
+  "Chat commands include",
    "@help",
    "@temp",
    '@weather',
@@ -10,8 +11,7 @@ var commands = [
    '@ar',
    '@fr',
    '@cn',
-   '@hk',
-   '@morehelp - describe each command'
+   '@hk'
 
 ];
 
@@ -31,19 +31,97 @@ var commandDescription = [
 ];
 
 function getHelp(){
+      $(".chatbody").append("<div class = 'returnedhelp'>"+commands.join(', ')+"</div>");
 
-   for(var i = 0; i < commands.length; i++){
-      $(".chatbody").append("<div class = 'returnedinput'>"+commands[i]+"</div>");
-
-   }
+      $(".chatbody").append("<div class = 'returnedhelp'>@morehelp + command (i.e. @morehelp @temp) - describe each command</div>");
 doWhatMyHeartTellsMe();
 };
 
 function getMoreHelp(){
+  var helpText = textArr[0];
 
-  for(var i = 0; i < commandDescription.length; i++){
-     $(".chatbody").append("<div class = 'returnedinput'>"+commandDescription[i]+"</div>");
+  switch(helpText){
 
-  }
-doWhatMyHeartTellsMe();
+     case '@temp':
+        $(".chatbody").append("<div class = 'returnedhelp'>@temp, followed by City and State, will return the current temperature of that location.</div>");
+     break;
+
+     case '@weather':
+       $(".chatbody").append("<div class = 'returnedhelp'> @weather followed by the city and state will give the weather forecast for that day</div>");
+      break;
+
+     case '@morehelp':
+        $(".chatbody").append("<div class = 'returnedhelp'> @morehelp brings these up. You know this, you just typed it in. A++</div>");
+     break;
+
+    case '@github':
+           $(".chatbody").append("<div class = 'returnedhelp'>@github followed by a github username will return that user's avatar and name </div>");
+    break;
+
+    case '@gif':
+    $(".chatbody").append("<div class = 'returnedhelp'>@gif followed by a keyword will return an amusing .gif for your entertainment. </div>");
+    break;
+
+    case '@calc':
+           $(".chatbody").append("<div class = 'returnedhelp'> @calc followed by an equation will solve that equation</div>");
+      break;
+
+    case '@pokedex':
+        $(".chatbody").append("<div class = 'returnedhelp'>@pokedex followed by a pokemon name will return a sweet shiny pokemon </div>");
+     break;
+
+     case '@fr':
+         $(".chatbody").append("<div class = 'returnedhelp'>@fr followed by a phrase translates that phrase into French </div>");
+     break;
+
+     case '@ar':
+         $(".chatbody").append("<div class = 'returnedhelp'>@ar followed by a phrase translates that phrase into Arabic </div>");
+     break;
+
+     case '@cn':
+         $(".chatbody").append("<div class = 'returnedhelp'>@cn followed by a phrase translates that phrase into Simplified Chinese </div>");
+     break;
+
+     case '@hk':
+      $(".chatbody").append("<div class = 'returnedhelp'>@hk followed by a phrase translates that phrase into Traditional Chinese </div>");
+     break;
+
+     case '@hr':
+         $(".chatbody").append("<div class = 'returnedhelp'>@hr followed by a phrase translates that phrase into Croatian </div>");
+     break;
+
+     case '@et':
+        $(".chatbody").append("<div class = 'returnedhelp'> @et followed by a phrase translates that phrase into Estonian</div>");
+     break;
+
+     case '@tl':
+        $(".chatbody").append("<div class = 'returnedhelp'>@tl followed by a phrase translates that phrase into Tagalog </div>");
+     break;
+
+     case '@hi':
+        $(".chatbody").append("<div class = 'returnedhelp'>@hi followed by a phrase translates that phrase into Hindi</div>");
+     break;
+
+     case '@is':
+       $(".chatbody").append("<div class = 'returnedhelp'>@is followed by a phrase translates that phrase into Icelandic </div>");
+     break;
+
+     case '@th':
+       $(".chatbody").append("<div class = 'returnedhelp'>@th followed by a phrase translates that phrase into Thai </div>");
+     break;
+
+     case '@tr':
+       $(".chatbody").append("<div class = 'returnedhelp'> @tr followed by a phrase translates that phrase into Turkish</div>");
+     break;
+
+     case '@vi':
+       $(".chatbody").append("<div class = 'returnedhelp'>@vi followed by a phrase translates that phrase into Vietnamese</div>");
+     break;
+       $(".chatbody").append("<div class = 'returnedhelp'></div>");
+
+    default:
+         $(".chatbody").append("<div class = 'returnedhelp'>"+helpText+" is not a valid command</div>");
+    break;
+
+  };
 };
